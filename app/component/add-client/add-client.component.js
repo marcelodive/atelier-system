b945648f7bbae74e24964e71f43859ecc64e3ebd
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-07-07 23:03:31 -0300

    Client form complete

--- app/component/add-client/add-client.component.js
+++ app/component/add-client/add-client.component.js
@@ -12,15 +12,31 @@ angular.module('App')
 function AddClientController () {
   const vm = this;
   vm.client = {children: [{}]};
+  vm.today = new Date();
 
   vm.cancelAddition = cancelAddition;
   vm.addChild = addChild;
+  vm.removeChild = removeChild;
+  vm.updateChildAge = updateChildAge;
+  vm.saveClient = saveClient;
 
   function addChild () {
     vm.client.children.push({});
   }
 
+  function removeChild (key) {
+    vm.client.children.splice(key,1);
+  }
+
   function cancelAddition () {
     vm.cancelCallback();
+  }
+
+  function updateChildAge (birthday, child) {
+    child.age = moment(vm.today).diff(birthday, 'years');
+  }
+
+  function saveClient () {
+    console.log('saved');
   }
 }
