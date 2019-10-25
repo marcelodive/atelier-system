cc037fe42c8d20598c3e2d8e117498a77dd80820
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-10-24 21:35:32 -0300

    New functions and some layout

--- app/component/orders-view/orders-view.component.js
+++ app/component/orders-view/orders-view.component.js
@@ -13,14 +13,38 @@ function OrdersViewController (orderFactory) {
   vm.loadOrders = loadOrders;
   vm.triggerAddingOrder = triggerAddingOrder;
   vm.cancelAddingOrder = cancelAddingOrder;
+  vm.reduceName = reduceName;
+  vm.formatDate = formatDate;
 
   function init () {
     loadOrders();
   }
 
+  function formatDate (date) {
+    return moment.utc(date).format('DD/MM/YYYY');
+  }
+
+  function reduceName (name) {
+    const splitedName = name.split(' ');
+    if (splitedName.length <= 2) {
+      return name;
+    } else {
+      const firstName = splitedName[0];
+      const middleNamesWithDot = splitedName.reduce(((namesWithDots, name, index) => {
+        if (index > 0 && index < (splitedName.length - 1)) {
+          namesWithDots = `${namesWithDots} ${name[0]}.`
+        }
+        return namesWithDots;
+      }), '');
+      const surName = splitedName[splitedName.length - 1];
+      return `${firstName} ${middleNamesWithDot} ${surName}`;
+    }
+  }
+
   async function loadOrders () {
     const {data:orders} = await orderFactory.loadOrders();
     vm.orders = orders;
+    vm.orders = [...orders, ...orders, ...orders]
   }
 
   function triggerAddingOrder () {
