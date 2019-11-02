b9db972e2f9f27d57ecc71ccce721a9e7acf5e44
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-11-02 19:41:54 -0300

    Configurations

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,12 @@
 
 echo 'Initiating sync'
 
-rsync -au --exclude '.git' --exclude 'datasources.json' --exclude 'deploy' ~/Developer/luiza-sales/ ~/Developer/7140-665b97281eaf860448cb6787d283d643/
+rsync -au \
+  --exclude '.git' \
+  --exclude '.gitignore' \
+  --exclude 'datasources.json' \
+  --exclude 'deploy' \
+  --exclude 'config.json' \
+  ~/Developer/luiza-sales/ ~/Developer/7140-665b97281eaf860448cb6787d283d643/
 
 echo 'Ended sync'
