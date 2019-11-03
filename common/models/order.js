561c9e7a99aad40fc4aabe1c9a6b3c13797564bc
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-11-03 04:06:02 -0300

    Telegram bot working :)

--- common/models/order.js
+++ common/models/order.js
@@ -1,27 +1,39 @@
 
 const app = require('../../server/server');
+const sendmail = require('sendmail')();
 
 module.exports = function(Order) {
   Order.saveOrder = async(order) => {
-    const {Installment} = app.models;
-    const {OrderProduct} = app.models;
-    const {Product} = app.models;
     order.cep = Number(String(order.cep).replace('-', ''));
     const savedOrder = await Order.upsert(order);
 
-    Installment.destroyAll({order_id: savedOrder.id});
+    saveOrderInstallments(order);
+    saveOrderProducts(order);
 
+    sendConfirmationEmailToCliente(order);
+
+    return savedOrder;
+  };
+
+  function saveOrderInstallments (order) {
+    const {Installment} = app.models;
+
+    Installment.destroyAll({order_id: savedOrder.id});
     order.installments.forEach((installment, index) => {
       installment.order_id = savedOrder.id;
       installment.payment_day = savedOrder.paymentDay[index];
       Installment.upsert(installment);
     });
+  }
+
+  function saveOrderProducts (order) {
+    const {OrderProduct} = app.models;
+    const {Product} = app.models;
 
     order.products.filter((product) => product.name)
       .forEach((product) => {
         product.order_id = savedOrder.id;
         product.name = product.searchText;
-
         OrderProduct.upsert(product);
 
         const productToUpdateOrAdd = (product.autocompleteItem) ?
@@ -29,9 +41,20 @@ module.exports = function(Order) {
           product;
         Product.upsert(productToUpdateOrAdd);
       });
+  }
 
-    return savedOrder;
-  };
+  function sendConfirmationEmailToCliente (order) {
+    sendmail({
+      from: 'test@yourdomain.com',
+      to: 'info@yourdomain.com',
+      replyTo: 'jason@yourdomain.com',
+      subject: 'MailComposer sendmail',
+      html: 'Mail of test sendmail '
+    }, function (err, reply) {
+      console.log(err && err.stack)
+      console.dir(reply)
+    })
+  }
 
   Order.remoteMethod('saveOrder', {
     accepts: {arg: 'order', type: 'Object'},
--- server/boot/ping.js
+++ server/boot/ping.js
@@ -1,6 +1,23 @@
+const sendmail = require('sendmail')();
+
 module.exports = function(app) {
   // Install a "/ping" route that returns "pong"
   app.get('/ping', (req, res) => {
     res.send('pong');
   });
+
+  // sendConfirmationEmailToCliente();
 };
+
+function sendConfirmationEmailToCliente () {
+  sendmail({
+    from: 'marcelo.rodrigues@mailinator.com',
+    to: 'atelieluizasales@yourdomain.com, marcelo.rodrigues@mailinator.com',
+    replyTo: 'marcelo.rodrigues@mailinator.com',
+    subject: 'MailComposer sendmail',
+    html: 'Mail of test sendmail '
+  }, function (err, reply) {
+    console.log(err && err.stack)
+    console.dir(reply)
+  });
+}
