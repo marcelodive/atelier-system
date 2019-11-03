68d85d8facf250f1de8dcc504879e2cbb83e9985
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-11-03 04:06:30 -0300

    Telegram bot working :)

---
+++ server/boot/telegram.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
+
+startTelegramBot();
+
+function startTelegramBot () {
+  const token = '944173870:AAEAOZ-Gq92u04d7PlxYH0anfaJmw0nmqC4';
+  const bot = new TelegramBot(token, {polling: true});
+
+  const telegramConfigFile = fs.readFileSync('./server/telegram.json');
+  const telegramConfig = JSON.parse(telegramConfigFile);
+
+  telegramConfig.users.forEach((user) => {
+    bot.sendMessage(msg.chat.id, 'Sistema iniciado com sucesso');
+  });
+
+  bot.on('message', (msg) => {
+    if (telegramConfig.users.includes(msg.chat.id)) {
+      bot.sendMessage(msg.chat.id, 'Seu usuário já está cadastrado no sistema de logs');
+    } else {
+      telegramConfig.users.push(msg.chat.id);
+      fs.writeFileSync('./server/telegram.json', JSON.stringify(telegramConfig));
+      bot.sendMessage(msg.chat.id, 'Seu usuário foi cadastrado com sucesso no sistema de logs');
+    }
+  });
+}
