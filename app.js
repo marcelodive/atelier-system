026d552760810c74a0d1a285d040d49430c98a1a
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-11-02 22:00:33 -0300

    Yahoo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-require('./server/server.js').start();
+const fs = require('fs');
+const configPath = './server/config.json';
+const configFile = require(configPath);
+
+configFile.host = process.env.app_host || 3000;
+configFile.port = process.env.app_port || '0.0.0.0';
+
+fs.writeFile(configPath, JSON.stringify(configFile), function (err) {
+  if (err) return console.log(err);
+  console.log(JSON.stringify(configFile));
+  console.log('writing to ' + configPath);
+  require('./server/server.js').start();
+});
+
