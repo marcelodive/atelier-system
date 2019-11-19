6cbb03e4b961a1eda5bb5a42d6b6b0c9a1b5f8ae
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-11-19 01:11:19 -0300

    Better constants

--- client/app.constants.js
+++ client/app.constants.js
@@ -5,6 +5,6 @@ angular.module('App')
   .constant('emailStatuses', {
     0: 'Email não enviado.',
     1: 'Email enviado',
-    2: 'Pedido recusado pelo cliente',
-    3: 'Pedido aceito pelo cliente'
+    2: 'Pedido aceito pelo cliente',
+    3: 'Pedido recusado pelo cliente',
   });
--- common/constants.js
+++ common/constants.js
@@ -1,4 +1,5 @@
 exports.emailStatuses = {
+  notSent: 0,
   sent: 1,
   accepted: 2,
   denied: 3,
